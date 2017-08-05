73890091cf661417e5024de2864a546a3ca24c1a
Meshan Naidoo <meshann@gmail.com>
2017-08-05 18:55:14 +0200

    initial

---
+++ BasicSocial.Domain/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace BasicSocial.Domain
+{
+    public class User
+    {
+		public int Id { get; set; }
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public string Age { get; set; }
+		public IList<User> Friends { get; set; }
+		public IList<Post> Posts { get; set; }
+    }
+
+	public class Post
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+	}
+}
