ec9270f5f5936245b10dc0a625a2b7fb435c2af7
Meshan Naidoo <meshann@gmail.com>
2017-08-06 18:39:22 +0200

    Add mvc web project

---
+++ BasicSocial.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BasicSocial.Web.Startup))]
+namespace BasicSocial.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
