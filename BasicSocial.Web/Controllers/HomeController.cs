d3a7744d25177049c035b5462a32e78788eda1fe
Meshan Naidoo <meshann@gmail.com>
2017-08-07 19:52:27 +0200

    Compose message working

--- BasicSocial.Web/Controllers/HomeController.cs
+++ BasicSocial.Web/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using System.Linq;
 using System.Web.Mvc;
+using BasicSocial.Core;
 using BasicSocial.Dal;
+using BasicSocial.Web.Models;
 using Microsoft.AspNet.Identity;
 
 namespace BasicSocial.Web.Controllers
@@ -39,7 +41,37 @@ namespace BasicSocial.Web.Controllers
 
 		public ActionResult Messages()
 		{
-			return View();
+			var userId = User.Identity.GetUserId<int>();
+			var user = Context.Users.FirstOrDefault(x => x.Id == userId);
+			return View(user);
+		}
+
+		public ActionResult ComposeMessage()
+		{
+			var userId = User.Identity.GetUserId<int>();
+			var user = Context.Users.FirstOrDefault(x => x.Id == userId);
+			return View(user);
+		}
+
+		[HttpPost]
+		public ActionResult ComposeMessage(ComposeMessageModel model)
+		{
+			var userId = User.Identity.GetUserId<int>();
+			var user = Context.Users.FirstOrDefault(x => x.Id == userId);
+			var user2 = Context.Users.FirstOrDefault(x => x.Id == model.ToUserId);
+
+			var privateMessage = new PrivateMessage()
+			{
+				Subject = model.Subject,
+				Content = model.Content,
+				Sender = user,
+				Receiver = user2
+			};
+
+			Context.PrivateMessages.Add(privateMessage);
+			Context.SaveChanges();
+
+			return View(user);
 		}
 	}
 }
---
+++ BasicSocial.Web/Models/ComposeMessageModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BasicSocial.Web.Models
+{
+	public class ComposeMessageModel
+	{
+		public string Subject { get; set; }
+		public string Content { get; set; }
+		public int ToUserId { get; set; }
+	}
+}
